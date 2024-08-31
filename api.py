008ca99b03ae0c5a639be51f1dd7c5f03698c91d
PR7175Z <aadimpraz@gmail.com>
2024-08-31 19:30:19 +0545

    Created API for prediction

---
+++ api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, conlist
+import pickle
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+app = FastAPI()
+
+with open('codes/model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+
+class feature(BaseModel):
+    features:conlist(float, min_length = 5, max_length = 5)
+
+@app.post('/predict')
+def predict(data: feature):
+    try:
+        # scaler = StandardScaler()
+
+        features = np.array(data.features).reshape(1, -1)
+        # features_reshape = scaler.fit_transform(features)
+        prediction = loaded_model.predict(features)
+        return (f'Prediction : {int(prediction)}')
+    except Exception as e:
+        return HTTPException(status_code = 500, detail = str(e))
