9a105b05c2be19ed55bc7f8b8843fb3cf8eb6357
PR7175Z <aadimpraz@gmail.com>
2024-09-13 22:14:24 +0545

    Added CORS

--- api.py
+++ api.py
@@ -3,8 +3,23 @@ from pydantic import BaseModel, conlist
 import pickle
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Can also be ["*"] to allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 with open('models/model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
@@ -15,7 +30,6 @@ class feature(BaseModel):
 @app.post('/predict')
 def predict(data: feature):
     try:
-        # scaler = StandardScaler()
 
         features = np.array(data.features).reshape(1, -1)
         # features_reshape = scaler.fit_transform(features)
