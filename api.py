83cb3aaefea6d1a5ac4549afa5dd107bbdb35e51
PR7175Z <aadimpraz@gmail.com>
2024-09-15 13:53:17 +0545

    Normalized api request data

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, conlist
-import pickle
+from sklearn.preprocessing import StandardScaler
+import joblib
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
@@ -21,8 +22,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-with open('models/model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
+loaded_model = joblib.load('models/academicperformanceprediction.pkl')
+
+# with open('models/academicperformanceprediction.pkl', 'rb') as f:
+#     loaded_model = pickle.load(f)
 
 class feature(BaseModel):
     features:conlist(float, min_length = 5, max_length = 5)
@@ -30,10 +33,10 @@ class feature(BaseModel):
 @app.post('/predict')
 def predict(data: feature):
     try:
-
+        scaler = StandardScaler()
         features = np.array(data.features).reshape(1, -1)
-        # features_reshape = scaler.fit_transform(features)
-        prediction = loaded_model.predict(features)
+        features_reshape = scaler.fit_transform(features)
+        prediction = loaded_model.predict(features_reshape)
         return (f'Prediction : {int(prediction)}')
     except Exception as e:
         return HTTPException(status_code = 500, detail = str(e))
