3c0b5fa5c5f762680873e45a23e0ea42939db01d
PR7175Z <aadimpraz@gmail.com>
2024-09-17 07:25:50 +0545

    Display container on response

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    const container = document.querySelector('.container');
     const resultDisplay = document.getElementById('result');
 
     //API fetching function
@@ -19,7 +20,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data);
         return data;
     }
 
@@ -27,13 +27,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('inputform').addEventListener('submit', (e) => {
         e.preventDefault();
         const name = document.querySelector('input[name="name"]').value;
-        const gender = document.querySelector('input[name="gender"]').value;
         const attendance = document.querySelector('input[name="attendance"]').value;
         const studyHour = document.querySelector('input[name="studyhour"]').value;
         const previousGrade = document.querySelector('input[name="previousgrade"]').value;
         const extraCurricularActivities = document.querySelector('input[name="extracurricularactivities"]').value;
         const parentalSupport = document.querySelector('select[name="parentalsupport"]').value;
-
 
         //asynchronous function for response
         async function displayResponse() {
@@ -43,9 +41,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 heading.innerHTML = `Hello ${name}`;
                 const response = await get_response( parseInt(parentalSupport), previousGrade, studyHour, extraCurricularActivities, attendance);
 
-                prediction.innerHTML = response
+                prediction.innerHTML = `Your academic score is predicted to be: ${response}`;
                 resultDisplay.appendChild(heading);
                 resultDisplay.appendChild(prediction);
+                container.style.display = 'block';
             } catch (error) {
                 console.error("Error:", error);
             }
