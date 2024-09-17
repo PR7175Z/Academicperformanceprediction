bf29783d2daaec1e4ccf73fe7ba7fa74d2efa9b2
PR7175Z <aadimpraz@gmail.com>
2024-09-17 07:12:45 +0545

    Display function created

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    async function get_response() {
-        const features = {'features': [0, 20, 10, 3, 51.6587823 ]};
+    const resultDisplay = document.getElementById('result');
+
+    //API fetching function
+    async function get_response(ParentalSupport, PreviousGrade, StudyHoursPerWeek, ExtracurricularActivities, AttendanceRate) {
+        const features = {'features': [ParentalSupport, PreviousGrade, StudyHoursPerWeek, ExtracurricularActivities, AttendanceRate ]};
 
         const api = 'http://127.0.0.1:8000/predict';
         const response = await fetch(api,{
@@ -17,7 +20,37 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
         const data = await response.json();
         console.log(data);
+        return data;
     }
 
-    get_response();
+    //eventlisterner for form submission
+    document.getElementById('inputform').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = document.querySelector('input[name="name"]').value;
+        const gender = document.querySelector('input[name="gender"]').value;
+        const attendance = document.querySelector('input[name="attendance"]').value;
+        const studyHour = document.querySelector('input[name="studyhour"]').value;
+        const previousGrade = document.querySelector('input[name="previousgrade"]').value;
+        const extraCurricularActivities = document.querySelector('input[name="extracurricularactivities"]').value;
+        const parentalSupport = document.querySelector('select[name="parentalsupport"]').value;
+
+
+        //asynchronous function for response
+        async function displayResponse() {
+            try {
+                const heading = document.createElement('h3');
+                const prediction = document.createElement('p');
+                heading.innerHTML = `Hello ${name}`;
+                const response = await get_response( parseInt(parentalSupport), previousGrade, studyHour, extraCurricularActivities, attendance);
+
+                prediction.innerHTML = response
+                resultDisplay.appendChild(heading);
+                resultDisplay.appendChild(prediction);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+
+        displayResponse();
+    })
 })
