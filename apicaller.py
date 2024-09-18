db2004fe26f331da6bff9533296d1300cfacb327
PR7175Z <aadimpraz@gmail.com>
2024-09-18 12:53:08 +0545

    Removed unnecessary file

--- apicaller.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# url = 'http://127.0.0.1:4444/predict'
-url = 'http://127.0.0.1:8000/predict'
-
-data = {'features': [0, 53.7948461, 18.23871036, 3, 51.6587823 ]}
-response = requests.post(url, json=data)
-
-print(response.json())
